d8901ae03b828003ebba19b88b5e0fff56637620
Pedro Dias <petermdias@gmail.com>
2018-02-05 11:13:58 +0000

    m

--- main.js
+++ main.js
@@ -7,12 +7,10 @@ function startCollector() {
   console.log('(shipode) Spawning collector process.');
   var collectorProcess = child_process.fork(__dirname + '/lib/collector/process');
 
-  /*
   setTimeout(function() {
     collectorProcess.kill('SIGHUP');
     startCollector();
   }, 3600 * 1000);
-  */
 }
 
 
