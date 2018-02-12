d1245e59eb4385eaba4cc56c6e2db00b1808ef9c
Pedro Dias <petermdias@gmail.com>
2018-02-12 13:08:40 +0000

    Child process lifespan

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
 var child_process = require('child_process');
-
+var collectorProcess;
 
 function startCollector() {
   console.log('(shipode) Spawning collector process.');
-  var collectorProcess = child_process.fork(__dirname + '/lib/collector/process');
+  collectorProcess = child_process.fork(__dirname + '/lib/collector/process');
 
   setTimeout(function() {
     collectorProcess.kill('SIGHUP');
@@ -13,6 +13,30 @@ function startCollector() {
   }, 3600 * 1000);
 }
 
+
+function exitHandler(options, err) {
+  collectorProcess.stop();
+  process.exit();
+}
+
+process.on('exit', exitHandler.bind(null, {
+  cleanup: true
+}));
+
+process.on('SIGINT', exitHandler.bind(null, {
+  exit: true
+}));
+
+process.on('SIGUSR1', exitHandler.bind(null, {
+  exit: true
+}));
+process.on('SIGUSR2', exitHandler.bind(null, {
+  exit: true
+}));
+
+process.on('uncaughtException', exitHandler.bind(null, {
+  exit: true
+}));
 
 console.log('(shipode) Shipper started.');
 startCollector();
